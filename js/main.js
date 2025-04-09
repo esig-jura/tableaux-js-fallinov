03ceee9d2685201fdf09baec578fdaaee8a82d89
Steve Fallet <steve.fallet@divtec.ch>
2025-04-09 14:26:14 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -46,22 +46,7 @@ const champNom = document.getElementById('nom');
 const champAge = document.getElementById('age');
 const champLocalite = document.getElementById('localite');
 
-formulaire.addEventListener('submit', function(event){
-    event.preventDefault(); // Stoppe l'envoi du formulaire
-
-    // Ajoute une nouvelle personne à la fin du tableau
-    personnes.push({
-        prenom: champPrenom.value,
-        nom: champNom.value,
-        age: champAge.value,
-        localite: champLocalite.value
-    })
-
-    // Réafficher le tableau avec la nouvelle personne
-    affichePersonnes();
-});
-
-
+// Déclaration des fonctions
 // Fonction qui affiche les personnes dans le tableau HTML
 function affichePersonnes () {
     // Vider le tableau
@@ -77,6 +62,23 @@ function affichePersonnes () {
             </tr>`;
     }
 }
+function ajouterNouvellePersonne (event) {
+    event.preventDefault(); // Stoppe l'envoi du formulaire
 
-// Appel de la fonction pour afficher les personnes
+// Ajoute une nouvelle personne à la fin du tableau
+    personnes.push({
+        prenom: champPrenom.value,
+        nom: champNom.value,
+        age: champAge.value,
+        localite: champLocalite.value
+    });
+
+// Réafficher le tableau avec la nouvelle personne
+    affichePersonnes();
+}
+
+// Gestion des événements
+formulaire.addEventListener('submit', ajouterNouvellePersonne);
+
+// Initialisation de l'application
 affichePersonnes();
